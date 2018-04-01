3842a7a3d8df84615e0ca445f4c71a821082c0f8
Nate Bhurinat Wangsutthitham <nate.bwangsut@gmail.com>
2018-04-01 17:05:14 +0700

    Update webframework
    - Now using JS to control the page rather than static HTML
    - Enabled a dynamic page rendering
    - Changes to default font
    - Change a navigation tab label from 'Support' to 'Blog'

--- js/main.js
+++ js/main.js
@@ -1,3 +1,70 @@
 $('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
+    $('#myInput').trigger('focus')
 })
+
+function addPageContent(cardHeader, cardTitle, cardText, cardFooter, cardAttachment = undefined) {
+
+    // Create content header
+    let contentHeader = document.createElement("div");
+    $(contentHeader)
+        .addClass("card-header")
+        .html(cardHeader);
+
+    // Create content body
+    let contentBody = document.createElement("div");
+    $(contentBody)
+        .addClass("card-body")
+        // Add title
+        .append($(document.createElement("h5"))
+            .addClass("card-title")
+            .html(cardTitle))
+        // Add card text
+        .append($(document.createElement("p"))
+            .addClass("card-text")
+            .html(cardText))
+        // Append more stuff if attachment is not === undefined
+        .append(typeof(cardAttachment) === "string" 
+            ? cardAttachment 
+            : "");
+
+    // Create content Footer
+    let contentFooter = document.createElement("div");
+    $(contentFooter)
+        .addClass("card-footer")
+        .addClass("text-muted")
+        .addClass("text-center")
+        .html(cardFooter);
+
+    // Create a card wrapper
+    let card = document.createElement("div");
+    $(card)
+        .addClass("row")
+        .addClass("section")
+        .append($(document.createElement("div"))
+            .addClass("col-sm-12")
+            .append($(document.createElement("div"))
+                .addClass("card")
+                .append(contentHeader)
+                .append(contentBody)
+                .append(contentFooter)
+            )
+        );
+
+    // Append to content section
+    $('#page-content').prepend(card);
+}
+
+let cardHeader  = "Page Creation"
+let cardTitle   = "The beginning of CloudyCraft"
+let cardBody    = 'With supporting text below as a natural lead-in to additional content.'
+let cardDate    = "Friday, 30 March 2018 01:12 UTC+7"
+
+addPageContent(cardHeader, cardTitle, cardBody, cardDate);
+
+let cardHeader1  = "Featured"
+let cardTitle1   = "Special title treatment"
+let cardBody1    = 'With supporting text below as a natural lead-in to additional content.'
+let cardDate1    = "Friday, 30 March 2018 01:30 UTC+7"
+let cardAttachment1 = '<a href="#" class="btn btn-primary">Go somewhere</a>'
+
+addPageContent(cardHeader1, cardTitle1, cardBody1, cardDate1, cardAttachment1);
